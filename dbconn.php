1cb30c2140ca67f5d326e246c65decff2b6b3442
matthawes <hawes.matt@gmail.com>
2020-01-30 13:18:33 -0500

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -10,8 +10,8 @@
         $conarr[$k] = substr($value,strpos($value,'=')+1);
     }
     // $conarr is an array of values of connection string
-    print_r($conarr[1]);
-    print_r($conarr[2]);
-    print_r($conarr[3]);
-    print_r($conarr[4]);
+    print($conarr[1]);
+    print($conarr[2]);
+    print($conarr[3]);
+    print($conarr[4]);
 ?>
