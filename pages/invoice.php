7087dcaa5b3877e9951c8317e566d38179f98d3c
matthawes <hawes.matt@gmail.com>
2020-02-04 16:11:36 -0500

    Update invoice.php

--- pages/invoice.php
+++ pages/invoice.php
@@ -24,9 +24,9 @@ if(!isset($_SESSION['user_id'])){
 	  
 	if ($_SERVER[REQUEST_METHOD]=="POST") {
 		
-		$selectValue=$_POST["idValue"];
+		$selectValue = $mysqli -> real_escape_string($_POST["idValue"]);
 
-		$invoice_query = "SELECT invoice_id, invoice_number, total_amount FROM invoice WHERE invoice_id=".$selectValue." ORDER BY invoice_id ASC";
+		$invoice_query = "SELECT invoice_id, invoice_number, total_amount FROM invoice WHERE invoice_id='".$selectValue."' ORDER BY invoice_id ASC";
 
 		$invoice_result = mysqli_query($connect, $invoice_query);
 		
