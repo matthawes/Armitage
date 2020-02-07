e842f4c69b4ddde81d4088cb498fd737967ae704
Matt Hawes <hawes.matt@gmail.com>
2020-02-07 16:31:51 +0000

    .

--- pages/invoice.php
+++ pages/invoice.php
@@ -6,7 +6,7 @@
     $selectValue = mysqli_real_escape_string($connect, $_POST["invoiceNum"]);
     $invoiceNum_query = "SELECT invoice_number FROM invoice WHERE invoice_number <> '' ORDER BY invoice_number ASC";
     $invoiceNum_result = mysqli_query($connect, $invoiceNum_query);
-    if ($_SERVER[REQUEST_METHOD]=="POST") {
+    if ($_SERVER['REQUEST_METHOD']=="POST") {
             $selectValue = mysqli_real_escape_string($connect, $_POST["invoiceNum"]);
 
             $invoice_query = "SELECT invoice.*, vendor.vendor_name, company.company_name, company.address_1, company.address_2, company.city, company.state, company.zip, term.term, payment_method.payment_method
@@ -19,14 +19,15 @@
             $invoice_result = mysqli_query($connect, $invoice_query);
             $selectedInvoice = mysqli_fetch_array($invoice_result);
 
-            $invoice_lines_query = "SELECT invoice.invoice_id, invoice.invoice_number, invoice_line.*
+            $invoice_lines_query = "SELECT invoice.invoice_id, invoice.invoice_number, invoice_line.*, food_item_cost.type
                     FROM invoice
                     INNER JOIN invoice_line ON invoice_line.invoice_id = invoice.invoice_id
+                    LEFT JOIN food_item_cost ON food_item_cost.food_item_cost_id = invoice_line.food_item_cost_id
                     WHERE invoice_number='".$selectValue."' ORDER BY invoice_line_id ASC";
                     $invoice_lines_result = mysqli_query($connect, $invoice_lines_query);
             
             while ($selectedInvoiceLines = mysqli_fetch_array($invoice_lines_result)){
-                echo $selectedInvoiceLines[0].$selectedInvoiceLines[1].$selectedInvoiceLines[2].$selectedInvoiceLines[3];
+                echo $selectedInvoiceLines['invoice_number'].$selectedInvoiceLines['invoice_line_id'].$selectedInvoiceLines['type'].$selectedInvoiceLines['amount'];
             }
     }
 ?>
@@ -53,11 +54,11 @@
                         <div class="row wow fadeInUp" data-wow-delay="0.2s">
                             <div class="col-4">
                                 <label class="control-label" for="memo">Bill To</label>
-                                <textarea class="form-control bg-yellow" rows="4" id="address" name="address"><?= $selectedInvoice[company_name] ?? '' ?>&#010;<?= $selectedInvoice[address_1] ?? '' ?>&#010;<?= $selectedInvoice[address_2] ?? '' ?>&#010;<?= $selectedInvoice[city] ?? '' ?> <?= $selectedInvoice[state] ?? '' ?> <?= $selectedInvoice[zip] ?? '' ?></textarea>
+                                <textarea class="form-control bg-yellow" rows="4" id="address" name="address"><?= $selectedInvoice['company_name'] ?? '' ?>&#010;<?= $selectedInvoice['address_1'] ?? '' ?>&#010;<?= $selectedInvoice['address_2'] ?? '' ?>&#010;<?= $selectedInvoice['city'] ?? '' ?> <?= $selectedInvoice['state'] ?? '' ?> <?= $selectedInvoice['zip'] ?? '' ?></textarea>
                             </div>
                             <div class="col-2">
                                 <label class="control-label" for="amountDue">Amount Due</label>  
-                                <input id="amountDue" name="amountDue" type="text" placeholder="<?= $selectedInvoice[total_amount] ?? '' ?>" class="bg-yellow form-control">
+                                <input id="amountDue" name="amountDue" type="text" placeholder="<?= $selectedInvoice['total_amount'] ?? '' ?>" class="bg-yellow form-control">
                             </div>
                             <div class="col-2">
                                 <label class="control-label" for="terms">Terms</label>  
@@ -82,23 +83,23 @@
                         <div class="row wow fadeInUp" data-wow-delay="0.4s">
                             <div class="col-2">
                                 <label class="control-label" for="vendor">Vendor</label>
-                                <input id="vendor" name="vendor" type="text" placeholder="<?= $selectedInvoice[vendor_name] ?? '' ?>" class="bg-yellow form-control">
+                                <input id="vendor" name="vendor" type="text" placeholder="<?= $selectedInvoice['vendor_name'] ?? '' ?>" class="bg-yellow form-control">
                             </div>
                             <div class="col-2">
 				<label class="control-label" for="date">Date</label>
-                                <input id="date" name="date" type="text" placeholder="<?= $selectedInvoice[invoice_date] ?? '' ?>" class="bg-yellow form-control">
+                                <input id="date" name="date" type="text" placeholder="<?= $selectedInvoice['invoice_date'] ?? '' ?>" class="bg-yellow form-control">
                             </div>
                             <div class="col-2">
                                 <label class="control-label" for="dueDate">Due Date</label>
-                                <input id="dueDate" name="dueDate" type="text" placeholder="<?= $selectedInvoice[due_date] ?? '' ?>" class="bg-yellow form-control">
+                                <input id="dueDate" name="dueDate" type="text" placeholder="<?= $selectedInvoice['due_date'] ?? '' ?>" class="bg-yellow form-control">
                             </div>
                             <div class="col-2">
                                 <label class="control-label" for="paymentMethod">Payment Method</label>
-                                <input id="paymentMethod" name="paymentMethod" type="text" placeholder="<?= $selectedInvoice[payment_method] ?? '' ?>" class="bg-yellow form-control">
+                                <input id="paymentMethod" name="paymentMethod" type="text" placeholder="<?= $selectedInvoice['payment_method'] ?? '' ?>" class="bg-yellow form-control">
                             </div>
                             <div class="col-4">
                                 <label class="control-label" for="memo">Memo</label>
-                                <input id="memo" name="memo" type="text" placeholder="<?= $selectedInvoice[memo] ?? '' ?>" class="bg-yellow form-control">
+                                <input id="memo" name="memo" type="text" placeholder="<?= $selectedInvoice['memo'] ?? '' ?>" class="bg-yellow form-control">
                             </div>
                         </div>
                         <hr class="wow fadeInRight" data-wow-delay="0.5s" style="visibility: visible; animation-delay: 0.3s;">
