aac9dee1e68567ebe0c1d195d1e804069bb372a2
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-07 21:17:36 -0600

    Update COGS.php

--- pages/COGS.php
+++ pages/COGS.php
@@ -47,9 +47,10 @@
 				 	 <tbody>
 				 		<tr>
                             <td width="30%" class="border-all text-center">Target Food Costs %</td>
-                            <td width="22%" class="bg-yellow border-all">&nbsp;</td>
-							<td width="25%" class="border-all text-center">Projected Food Costs %</td>
-                            <td width="25%" class="bg-yellow border-all">&nbsp;</td>
+                            <td width="20%" class="bg-yellow border-all">&nbsp;</td>
+							<td width="10%"></td>
+							<td width="30%" class="border-all text-center">Projected Food Costs %</td>
+                            <td width="20%" class="bg-yellow border-all">&nbsp;</td>
 						</tr>
 					</tbody>
 			<div class="col-8">
@@ -58,6 +59,7 @@
 						<tr>	
                             <td width="30%" class="border-all text-center">Target Alcohol Costs %</td>
                             <td width="20%" class="bg-yellow border-all">&nbsp;</td>
+							<td width="10%"></td>
                             <td width="30%" class="border-all text-center">Projected Alcohol Costs %</td>
                             <td width="20%" class="border-all">&nbsp;</td>
 						</tr>
