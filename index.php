7e644fd714bba05328e216954ac7f8a023954e46
matthawes <hawes.matt@gmail.com>
2020-02-09 17:10:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -115,5 +115,18 @@ echo $message;
     }
 
 </script>
+<?php 
+    //below  will give the whole connectionstring in a single string
+    $conn = getenv("MYSQLCONNSTR_localdb"); 
 
+    //Let's split it and decorate it in an array
+    $conarr2 = explode(";",$conn); 
+    $conarr = array();
+    foreach($conarr2 as $key=>$value){
+        $k = substr($value,0,strpos($value,'='));
+        $conarr[$k] = substr($value,strpos($value,'=')+1);
+    }
+    // $conarr is an array of values of connection string
+    print_r($conarr); 
+?>
 <?php include "pages/footer.php"; ?>
