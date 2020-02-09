d32a4e8e24e925860da1d311f4b9fc0cab7df2e8
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-09 10:38:15 -0600

    Update COGS.php

--- pages/COGS.php
+++ pages/COGS.php
@@ -34,7 +34,7 @@
 		 		   </div>
 		</div>
       </div>
-	  <div class="container" style="padding-top:5%">
+	  <div class="container1" style="padding-top:5%">
 	  	   <div class="row">
           		<div style="float: left; padding-left: 5%; padding-right: 5%; padding-bottom:2%" class="col-6 text-center">	
              	 	 <table border="1" cellspacing="0" cellpadding="0" class="table">	
@@ -65,7 +65,7 @@
 					</table>
 				</div>		
 			</div>	
-		<div class="container">
+		<div class="container1">
 			<div class=""row">
 		 		 <div style="float: left; padding-left: 5%; padding-right: 5%; padding-bottom:2%" class="col-6 text-center">
 		 	  	 	<table border="1" cellspacing="0" cellpadding="0" class="table">
