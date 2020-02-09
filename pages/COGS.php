a3d2266c84dabbbb2da1068ea82a59c70cc37715
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-09 08:54:40 -0600

    Update COGS.php

--- pages/COGS.php
+++ pages/COGS.php
@@ -22,49 +22,51 @@
         <div class="section-header wow fadeInUp" style="visibility: visible;">
           <h3 class="section-title">COGS</h3>
           	  <span class="section-divider"></span>
-         <div class="input-container">
-			<div id="dateDownOne" onClick="decreaseDateByOne()">
-				<b>Previous</b>
-		 </div>			
+         	  		<div class="input-container">
+					<div id="dateDownOne" onClick="decreaseDateByOne()">
+					<b>Previous</b>
+		 			</div>			
 			<form id="dateForm">
 				<input type="date" class="tallInput" id="date" onblur="clearPage()">
 			</form>
-		 <div id="dateUpOne" onClick="increaseDateByOne()">
-			<b>Next</b>
-		 </div>
+		 		   <div id="dateUpOne" onClick="increaseDateByOne()">
+				   <b>Next</b>
+		 		   </div>
 		</div>
        </div>
       </div>
-	  <div class="row">
-          	<div style="float: left; class="col-sm text-center" >	
-             	 <table border="1" cellspacing="0" cellpadding="0" class="table table-responsive table-sm">	
-					<tbody>	
-						<tr>
-                            <th width="60%" class="border-all text-center">Target Food Costs %</th>
-                            <td width="40%" class="bg-yellow border-all">&nbsp;</td>
-						</tr>
-						<tr>
-							<th width="60%" float="left" class="border-all text-center">Projected Food Costs %</th>
-                            <td width="40%" class="border-all">&nbsp;</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-			<div style="float: right; class="col-sm text-center">
-				 <table border="1" cellspacing="0" cellpadding="0" class="table table-responsive table-sm">	
-				 	<tbody>
-						<tr>	
-                            <th width="60%" class="border-all text-center">Target Alcohol Costs %</th>
-                            <td width="40%" class="bg-yellow border-all">&nbsp;</td>
-						</tr>
-						<tr>
-                            <th width="60%" class="border-all text-center">Projected Alcohol Costs %</th>
-                            <td width="40%" class="border-all">&nbsp;</td>
-						</tr>
-                    </tbody>
-				</table>
-			</div>		
-		</div>	
+	  <div class="container-fluid"></div>
+	  	   <div class="row">
+          		<div style="float: left; class="col-sm text-center" >	
+             	 	 <table border="1" cellspacing="0" cellpadding="0" class="table table-responsive table-sm">	
+					 		<tbody>	
+									<tr>
+                            			<th width="60%" class="border-all text-center">Target Food Costs %</th>
+                           				<td width="40%" class="bg-yellow border-all">&nbsp;</td>
+									</tr>
+									<tr>
+										<th width="60%" float="left" class="border-all text-center">Projected Food Costs %</th>
+                            			<td width="40%" class="border-all">&nbsp;</td>
+									</tr>
+							</tbody>
+					</table>
+				</div>
+				<div style="float: right; class="col-sm text-center">
+					<table border="1" cellspacing="0" cellpadding="0" class="table table-responsive table-sm">	
+				 		   <tbody>
+						   		  <tr>	
+                            	  	  <th width="60%" class="border-all text-center">Target Alcohol Costs %</th>
+                           			  <td width="40%" class="bg-yellow border-all">&nbsp;</td>
+								  </tr>
+								  <tr>
+                            	  	  <th width="60%" class="border-all text-center">Projected Alcohol Costs %</th>
+                            		  <td width="40%" class="border-all">&nbsp;</td>
+								  </tr>
+                   			</tbody>
+					</table>
+				</div>		
+			</div>	
+		</div>
 
     </section>
 
