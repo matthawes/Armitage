517a6a40c96acad5abb11eb4058be5e50453fe50
Matt Hawes <hawes.matt@gmail.com>
2020-02-11 16:40:34 +0000

    .

--- pages/add_invoice.php
+++ pages/add_invoice.php
@@ -3,8 +3,11 @@
 <?php if(!isset($_SESSION['user_id'])){header("Location: ../index.php");} ?>
 <?php include "navigation.html"; ?>
 <?php
-            $company_name_query = "SELECT company_id, company_name FROM company";
-            $company_name_result = mysqli_query($connect, $company_name_query);
+    $company_name_query = "SELECT company_id, company_name FROM company";
+    $company_name_result = mysqli_query($connect, $company_name_query);
+
+    $terms_query = "SELECT term_id, term FROM term";
+    $terms_result = mysqli_query($connect, $terms_query);
 ?>
     <main id="main">
         <section id="services">
@@ -36,7 +39,14 @@
                             </div>
                             <div class="col-2">
                                 <label class="control-label" for="terms">Terms</label>  
-                                <input id="terms" name="terms" type="text" placeholder="" class="bg-yellow form-control"> 
+                                <select id="term" name="term" class="bg-yellow form-control">
+                                    <option value="">Select Term</option>
+                                    <?php
+                                        while ($terms = mysqli_fetch_array($terms_result)){
+                                            echo "<option value='".$terms[0]."'>".$terms[1]."</option>";
+                                        }
+                                    ?>
+                                </select>
                             </div>
                             <div class="col-4">
 				<label class="control-label" for="invoiceNum">Invoice #</label><br/>
