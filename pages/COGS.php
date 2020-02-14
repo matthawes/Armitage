4e8d9a1293a48fd6f6128fb9960a083d500dae6a
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-13 18:23:47 -0600

    Update COGS.php

--- pages/COGS.php
+++ pages/COGS.php
@@ -7,9 +7,6 @@
     header("Location: ../index.php");
 	} ?>
 
-
-<body>
-
   <main id="main">
 
     <!--==========================
@@ -160,57 +157,12 @@
 			</div>		
 		 </div> 
     </section>	
-	<script>
-	<?php
-	$invoice_query="SELECT 
-		<table border="1">
-			<tr>
-				<th>Date</th>
-				<th>Vendor</th>
-				<th>Amount</th>
-	</script>
+	
   </main>
 
 
 
-  <!-- JavaScript Libraries -->
-
-  <script src="../lib/jquery/jquery.min.js"></script>
-
-  <script src="../lib/jquery/jquery-migrate.min.js"></script>
-
-  <script src="../lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <script src="../lib/easing/easing.min.js"></script>
-
-  <script src="../lib/superfish/hoverIntent.js"></script>
-
-  <script src="../lib/superfish/superfish.min.js"></script>
-
-  <script src="../lib/wow/wow.min.js"></script>
-
-  <script src="../lib/waypoints/waypoints.min.js"></script>
-
-  <script src="../lib/counterup/counterup.min.js"></script>
-
-  <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
-
-  <script src="../lib/isotope/isotope.pkgd.min.js"></script>
-
-  <script src="../lib/lightbox/js/lightbox.min.js"></script>
-
-  <script src="../lib/touchSwipe/jquery.touchSwipe.min.js"></script>
-
-  <!-- Contact Form JavaScript File -->
-
-  <script src="../contactform/contactform.js"></script>
-
-
-
-  <!-- Template Main Javascript File -->
-
-  <script src="../js/main.js"></script>
-</body>
+ 
 
 <?php include "footer.php"; ?>
 
