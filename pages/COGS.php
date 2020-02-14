78df3cf04f02966d3ca1c957800cbd33d4df3d44
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-13 18:19:15 -0600

    Update COGS.php

--- pages/COGS.php
+++ pages/COGS.php
@@ -131,6 +131,11 @@
 									 <th width="60%" class="border-all bg-yellow text-center">Vendor</th>
 									 <th width="20%" class="border-all bg-yellow text-center">Amount</th>
 					 			 </tr>
+								 <tr>
+								 	 <td><input type="text" size="5" /></td>
+								 	 <td><input type="text" size="20" /></td>
+								 	 <td><input type="text" size="5" /></td>
+								 </tr>
 							</tbody>	 
 					 </table>
 			     </div>   
@@ -143,7 +148,11 @@
 								   	  <th width="60%" class="border-all bg-yellow text-center">Vendor</th>
 								   	  <th width="20%" class="border-all bg-yellow text-center">Amount</th>
 								  </tr>
-
+								  <tr>
+								 	  <td><input type="text" size="5" /></td>
+								 	  <td><input type="text" size="20" /></td>
+								 	  <td><input type="text" size="5" /></td>
+								 </tr>
 					 		</tbody>
 			  		 </table>
 										</tr>
