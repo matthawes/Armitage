d086965791139aa30972c85df24830f13eb97c7f
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-16 09:37:57 -0600

    Update COGS.php

--- pages/COGS.php
+++ pages/COGS.php
@@ -37,7 +37,7 @@
 					 		<tbody>	
 									<tr>
                             			<th width="80%" class="border-all text-center">Target Food Costs %</th>
-                           				<td width="20%" placeholder="<?= $target_result ?>"; class="bg-yellow border-all"></td>
+                           				<td width="20%" placeholder="<?= $target_result[] ?>"; class="bg-yellow border-all"></td>
 									</tr>
 									<tr>
 										<th width="80%" float="left" class="border-all text-center">Projected Food Costs %</th>
