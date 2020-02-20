4db9349c3d0b8c4db5df81aee5e4dac7f17237f3
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-19 18:11:36 -0600

    Update COGS.php

--- pages/COGS.php
+++ pages/COGS.php
@@ -40,7 +40,7 @@
         <div class="section-header wow fadeInUp" style="visibility: visible;">
           <h3 class="section-title">COGS</h3>
           	  <span class="section-divider"></span>
-         	  <div class="input-container">			
+         	  <div class="col-4">			
 			<label class="control-label" for="cogDate">COG Date</label><br/>
                                 <select id="cogDate" name="cogDate" class="bg-yellow form-control col-6" style="display:inline;">
                                     <option value="">Select a Date</option>
