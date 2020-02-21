a54444ce52f5437dd50603486616d24987e17e8d
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-20 18:13:39 -0600

    Update Dashtest.php

--- pages/Dashtest.php
+++ pages/Dashtest.php
@@ -6,20 +6,7 @@ if(!isset($_SESSION['user_id'])){
     header("Location: ../index.php");
 }
 ?>
-// A click on a week number cell in the weeks column of the start datepicker
-<?php 
-/* $("td.ui-datepicker-week-col").live("click", function()
-{
-    // Simulate a click on the first day of the week
-    $(this).next().click();
 
-    // Grab the date, and set the end of the week in the end datepicker
-    var fromDate = $("#inputStartDate").datepicker("getDate");    
-    var toDate = fromDate;
-    toDate.setDate(fromDate.getDate() + 6);
-    $("#inputEndDate").datepicker("setDate", toDate);
-}); */
-?> 
 <body>
 
 
