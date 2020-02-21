c07877420978184c4ecc48ba6aa41f5082092172
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-20 20:35:42 -0600

    Update Dashtest.php

--- pages/Dashtest.php
+++ pages/Dashtest.php
@@ -12,7 +12,7 @@ if(!isset($_SESSION['user_id'])){
 ?>
 <?php 
 $mydate=getdate(date_ISO8601("U")); 
-echo "$mydate[month]/$mydate[mday]"; 
+$mydate_result="$mydate[month]/$mydate[mday]"; 
 ?>
 
 <script type="text/javascript">
@@ -309,7 +309,7 @@ function toggleDataSeries(e) {
 
               <td scope="col" class="noborder"><a href="#"><i class="fa fa-2x fa-arrow-circle-left"></i></a></td>
 
-              <th scope="col" placeholder="$mydate"></th>
+              <th scope="col" placeholder="echo $mydate_result"></th>
 
               <th scope="col" class="dashDate">12/26</th>
 
