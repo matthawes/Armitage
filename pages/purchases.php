245747d615280c3acbf55806ce9c1d4ac2ecc505
cmluallen72 <60390483+cmluallen72@users.noreply.github.com>
2020-02-22 14:07:04 -0800

    Add files via upload

--- pages/purchases.php
+++ pages/purchases.php
@@ -82,6 +82,7 @@ if(!isset($_SESSION['user_id'])){
 			  <hr>
 			 <form>
 				  <div class="form-group w-50">
+                       <a class="btn btn-warning btn-lg" href="#" role="button">Submit</a>
 					<select class="form-control" id="exampleFormControlSelect1">
 					   <option selected="selected">Select...</option>
 					   <option value="Advertising">Advertising</option>
@@ -90,8 +91,7 @@ if(!isset($_SESSION['user_id'])){
 					   <option value="Office Supplies">Office Supplies</option>
 					   <option value="Repair and Maintenance">Repair and Maintenance</option>
                        <option value="Restaurant Supplies">Restauraunt Supplies</option>
-                    </select>
-                             <a class="btn btn-warning btn-lg" href="#" role="button">Submit</a>    
+                    </select>                                
 				  </div>
 			</form>                
 		    <table border="0" cellspacing="0" cellpadding="0" class="table table-bordered">
