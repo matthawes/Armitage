1e74c54091593063d11b881e30851829db735125
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-22 19:50:56 -0600

    Update Dashtest.php

--- pages/Dashtest.php
+++ pages/Dashtest.php
@@ -11,11 +11,7 @@ if(!isset($_SESSION['user_id'])){
 }
 ?>
 
-<?php
- (int)$currentpage = (!empty($_GET["currentpage"]))?$_GET["currentpage"] :0;
- (int)$nextpage = $currentpage + 1;
- (int)$prevpage = currentpage - 1;
-?>
+
 
 
 <body>
@@ -24,26 +20,34 @@ if(!isset($_SESSION['user_id'])){
 
 
 
-      <table>
-          <tr>
-              <td><a href="<?php echo "{$SERVER['PHP_SELF']}?currentpage=$prevpage"; ?>"> << </a></td>
-              <td></td>
-              <td><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$nextpage"; ?>"> >> </a></td>
-          </tr>
-          <tr>
-              <?php
-                    $ts = date(strtotime('last sunday'));
-                    $ts += $currentpage * 86400 * 7;
-                    $dow = date('w', $ts);
-                    $offset = $dow;
-                    
-                    $ts = $ts - $offset * 86400;
-                    for ($x=0; $x<7; $x++, $ts += 86400) {
-                        echo '<td>' . date("m/d/Y, $ts) . '</td>';
-                    }
-              ?>
-          </tr>
-      </table>
+    <?php
+
+    (int)$currentpage = (!empty($_GET["currentpage"]))?$_GET["currentpage"]:0;
+    (int)$nextpage = $currentpage + 1;
+    (int)$prevpage = $currentpage - 1;
+
+?>
+
+<table>
+    <tr>
+        <td><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$prevpage"; ?>"> << </a> </td>
+        <td></td>
+        <td><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$nextpage"; ?>"> >> </a> </td>
+    </tr>
+    <tr>
+        <?php
+            $ts = date(strtotime('last sunday'));
+            $ts += $currentpage * 86400 * 7;
+            $dow = date('w' , $ts);
+            $offset = $dow;
+
+            $ts = $ts - $offset * 86400;
+            for ($x=0 ; $x<7 ; $x++,$ts += 86400) {
+                echo '<td>' . date("m-d-Y", $ts) . '</td>' ;
+            }
+        ?>
+    </tr>
+</table>
   </main>
 
 </body>
