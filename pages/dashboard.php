9e69381e47ccd28f255ca7b69424545daa2470b4
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-22 20:13:50 -0600

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -164,7 +164,7 @@ function toggleDataSeries(e) {
         <table width="100%" border="0" cellspacing="0" cellpadding="0" class="text-center table table-bordered table-sm table-hover table-responsive-lg wow fadeInUpBig">
 	   <tr>
 	        <td scope="col" class="noborder"><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$prevpage"; ?>"><i class="fa fa-2x fa-arrow-circle-left"></i></a> </td>
-		<td scope="col" class="noborder"></td>
+		<th scope="col" class="noborder"></td>
 		<th scope="col"></th>
            	<th scope="col"></th>
         	<th scope="col"></th>
@@ -173,7 +173,7 @@ function toggleDataSeries(e) {
               	<th scope="col"></th>
               	<th scope="col"></th>
               	<td scope="col" class="noborder"><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$nextpage"; ?>"><i class="fa fa-2x fa-arrow-circle-right"></i></a></td>
-              	<td scope="col" class="noborder"></td>
+              	<th scope="col" class="noborder"></td>
 	   </tr>
 	    <tr>
 	        <?php
