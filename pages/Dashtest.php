cc98019627c0d3feeab72a83f2a68c5102eb574d
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-22 19:34:14 -0600

    Update Dashtest.php

--- pages/Dashtest.php
+++ pages/Dashtest.php
@@ -19,13 +19,31 @@ if(!isset($_SESSION['user_id'])){
   <main id="main">
 
 <?php
- for($i=0; $i<6; $i++){
-
-echo date('m-d-Y: D',strtotime("$i -day"))."<br>";
-
-}
+ (int)$currentpage = (!empty($_GET["currentpage"]))?$GET["currentpage"]:0;
+ (int)$nextpage = $currentpage + 1;
+ (int)$previouspage = currentpage - 1;
 ?>
 
+      <table>
+          <tr>
+              <td><a href="<?php echo "{$SERVER['PHP_SELF']}?currentpage=$previouspage"; ?>"> << </a></td>
+              <td></td>
+              <td><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$nextpage"; ?>"> >> </a></td>
+          </tr>
+          <tr>
+              <?php
+                    $ts = date(strtotime('last sunday'));
+                    $ts += $currentpage * 86400 * 7;
+                    $dow = date('w' , $ts);
+                    $offset = $dow;
+                    
+                    $ts = $ts - $offset * 86400;
+                    for ($x=0 ; $x<7 ; $x++,$ts += 86400) {
+                        echo "<td>' . dae("m/d/Y, $ts) . '</td>' ;
+                    }
+              ?>
+          </tr>
+      </table>
   </main>
 
 </body>
