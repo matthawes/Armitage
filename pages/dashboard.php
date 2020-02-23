c7d375ee486845efe0c187c09e76b16bdf6d237c
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-22 23:08:44 -0600

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -163,8 +163,8 @@ function toggleDataSeries(e) {
 	   <thead class="thead-warning">
 	    
 		   <tr>
-		  	<td><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$prevpage"; ?>"><i class="fa fa-2x fa-arrow-circle-left"></i></a> </td> 
-			<td scope="col" width="10%"></td>
+		  	<th><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$prevpage"; ?>"><i class="fa fa-2x fa-arrow-circle-left"></i></a> </th> 
+			<td></td>
 			   
       	        <?php
 	            $ts = date(strtotime('last sunday'));
