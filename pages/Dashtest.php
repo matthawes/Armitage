b53ed423a214fa00f0b00127621438a8e95c9807
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-22 21:57:37 -0600

    Update Dashtest.php

--- pages/Dashtest.php
+++ pages/Dashtest.php
@@ -330,7 +330,7 @@ function toggleDataSeries(e) {
 
 	   <tr>
 
-		   <td></td>
+		 
 
 		    <th></th>
 
@@ -361,7 +361,7 @@ function toggleDataSeries(e) {
 	    </tr>
  	<thead class="thead-warning"> 
 	  <tr>
-		  <td></td> 
+		  
 		  <th></th>
 		  <td>TOTAL</td>
               <?php
