fdb44032550295165a432a8accd294531bd40eb7
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-22 22:39:15 -0600

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -172,7 +172,7 @@ function toggleDataSeries(e) {
 	            $dow = date('w' , $ts);
 	            $offset = $dow;
 	            $ts = $ts - $offset * 86400;
-	            for ($x=0 ; $x<7 ; $x++,$ts += 86400) {
+	            for ($x=0 ; $x<7 ; $x++, $ts += 86400) {
 	                echo '<th>' . date("m-d-Y", $ts) . '</th>' ;
 		       }
 	        ?>
