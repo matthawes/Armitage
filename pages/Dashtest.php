83738096279b7ca99b28a5e434550bdf79e58d11
gfossler <60489205+gfossler@users.noreply.github.com>
2020-02-24 08:25:56 -0600

    Update Dashtest.php

--- pages/Dashtest.php
+++ pages/Dashtest.php
@@ -145,7 +145,7 @@ function toggleDataSeries(e) {
 	          <span class="section-divider"></span>
         </div>
         <table width="100%" border="0" cellspacing="0" cellpadding="0" class="text-center table table-bordered table-sm table-hover table-responsive-lg wow fadeInUpBig">
-		<form action="#" method="post">
+		/*<form action="#" method="post">
 			<div>
 				<label for="week">Which Week Would You LikeTo Start With?</label>
 				<input id="week" type="week" name="week" required>
@@ -154,16 +154,16 @@ function toggleDataSeries(e) {
 			<div>
 				<input type="submit" name="submit" value="Submit">
 				</div>
-		</form>
+		</form> */
 	   <thead class="thead-warning">
 	  	<td><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$prevpage"; ?>"><i class="fa fa-2x fa-arrow-circle-left"></i></a> </td> 
              	<td><a href="<?php echo "{$_SERVER['PHP_SELF']}?currentpage=$nextpage"; ?>"><i class="fa fa-2x fa-arrow-circle-right"></i></a></td>
 	        <tr>
 		   <th align="left"></th>
       	        <?php
-		    if ($_GET['submit']) {
+		    /* if ($_GET['submit']) {
 			    $week = $_POST['week'];
-			    $year = $POST['year'];
+			    $year = $POST['year']; */
 	            $ts = date(strtotime($week, $year));
 	            $ts += $currentpage * 86400 * 7;
 	            $dow = date('w' , $ts);
