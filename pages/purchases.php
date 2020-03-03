cc5d80f7867c32bf14a9d67a74adf3add360c925
gfossler <60489205+gfossler@users.noreply.github.com>
2020-03-02 18:37:55 -0600

    Update purchases.php

--- pages/purchases.php
+++ pages/purchases.php
@@ -189,6 +189,7 @@ if(!isset($_SESSION['user_id'])){
                         $purchaseOption = "SELECT alcohol_inventory.*, vendor.vendor_name FROM alcohol_inventory
                         LEFT JOIN vendor ON vendor.vendor_id = alcohol_inventory.vendor_id";
                         $purchase_result = mysqli_query($connect, $purchaseOption);
+                        }
                         
 		    <table border="0" cellspacing="0" cellpadding="0" class="table table-bordered">
 			 <thead class="thead-dark">
