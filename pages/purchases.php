7008171d7bad28ab85533460697b564aa0159713
gfossler <60489205+gfossler@users.noreply.github.com>
2020-03-03 21:33:36 -0600

    Update purchases.php

--- pages/purchases.php
+++ pages/purchases.php
@@ -190,8 +190,7 @@ if(!isset($_SESSION['user_id'])){
                         } */
                         if($option == "alcohol")
                         {
-                        $purchaseOption = "SELECT alcohol_inventory.*, vendor.vendor_name FROM alcohol_inventory, vendor
-                        JOIN vendor ON vendor.vendor_id = alcohol_inventory.vendor_id";
+                        $purchaseOption = "SELECT vendor_name FROM vendor";
                         $purchase_result = mysqli_query($connect, $purchaseOption);
                         }
                         /* else if($option == "advertising")
