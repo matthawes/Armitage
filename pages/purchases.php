c85a822ffbb27fafc7d5e826438d04c92e00aced
gfossler <60489205+gfossler@users.noreply.github.com>
2020-03-03 21:22:37 -0600

    Update purchases.php

--- pages/purchases.php
+++ pages/purchases.php
@@ -191,7 +191,7 @@ if(!isset($_SESSION['user_id'])){
                         if($option == "alcohol")
                         {
                         $purchaseOption = "SELECT alcohol_inventory.cost, vendor.vendor_name FROM alcohol_inventory, vendor
-                        LEFT JOIN vendor ON vendor.vendor_id = alcohol_inventory.vendor_id";
+                        
                         $purchase_result = mysqli_query($connect, $purchaseOption);
                         }
                         /* else if($option == "advertising")
