267e0707b187ac08400d00f633f7d500b49c3d11
Matt Hawes <hawes.matt@gmail.com>
2020-03-06 02:35:29 +0000

    .

--- pages/invoice.php
+++ pages/invoice.php
@@ -27,8 +27,8 @@
                     $invoice_lines_result = mysqli_query($connect, $invoice_lines_query);
     }
 ?>
-    <div class="navbar bg-yellow">
-        <div style="text-align: right; width: 100%; border-bottom: 1px solid gray;">You are viewing: <strong><?php echo($_SESSION['company_name']); ?></strong></div>
+    <div class="navbar bg-yellow" style="border-bottom: 1px solid gray;">
+        <div style="text-align: right; width: 100%;">You are viewing: <strong><?php echo($_SESSION['company_name']); ?></strong></div>
     </div>
     <main id="main">
         <section id="services">
