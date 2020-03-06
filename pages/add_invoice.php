4d907ac175e66b12cb747f6fdcdd96138c74acce
Matt Hawes <hawes.matt@gmail.com>
2020-03-06 03:26:15 +0000

    .

--- pages/add_invoice.php
+++ pages/add_invoice.php
@@ -140,7 +140,7 @@
                                 </table>
                             </div>
                         </div>
-                        <input onclick="alert('Validation will happen here soon.')" class="btn btn-warning btn-md" type="submit" name="submit" value="Save Invoice">
+                        <input onclick="alert(document.getElementById('amountDue').value + document.getElementById('amount1').value + document.getElementById('amount2').value + document.getElementById('amount3').value)" class="btn btn-warning btn-md" type="submit" name="submit" value="Save Invoice">
                     </fieldset>
                 </form>
             </div>
