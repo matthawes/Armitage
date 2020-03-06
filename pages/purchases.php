eb521f8cada97029f6bebc59de6aca85c8f9bde5
gfossler <60489205+gfossler@users.noreply.github.com>
2020-03-05 20:55:34 -0600

    Update purchases.php

--- pages/purchases.php
+++ pages/purchases.php
@@ -228,7 +228,7 @@ if(!isset($_SESSION['user_id'])){
 			       <tbody>
 			         <tr>
 			           <?php 
-                                
+                                if(isset($_POST['submit']))
                                    while($selectedPurchase = mysqli_fetch_array($purchase_result)){
                                         echo "<td>".$selectedPurchase['date']."</td><td>".$selectedPurchase['vendor_name']."</td><td>".$selectedPurchase['cost']."</td>";
                                         }
