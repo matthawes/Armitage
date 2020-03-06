a8db2d686d3726ed2c4dfc135136507d848d0d7d
gfossler <60489205+gfossler@users.noreply.github.com>
2020-03-05 21:31:46 -0600

    Update purchase1.php

--- pages/purchase1.php
+++ pages/purchase1.php
@@ -17,198 +17,73 @@ if(!isset($_SESSION['user_id'])){
       About Us Section
     ============================-->
     <section id="about">
-		<div class="container">
-			<header class="section-header">
+	<div class="container">
+		<header class="section-header">
 			<h3>Purchases</h3>
 			<span class="section-divider"></span>
 				<div class="input-container">
-					<div id="dateDownOne" onClick="decreaseDateByOne()">
-                    <b>Previous</b>
-					</div>	
-                <form id="dateForm">
-                    <input type="date" class="tallInput" id="date" onblur="clearPage()">
-                </form>
-					<div id="dateUpOne" onClick="increaseDateByOne()">
-                    <b>Next</b>
-					</div>
-           
-            
-            <style>
-			.rollInput {width:50%;}
-			#dateForm {text-align:center; display:table;}
-			#dateUpOne {display:table;}
-			#dateDownOne {display:table;}
-			.input-container {padding:5px; align: center; width: 25%; margin:auto; display:grid; grid-template-columns: 1fr 1fr 1fr;}
-			.container {overflow:hidden;}
-			#startingCash {border:2px solid #000; margin:0px 0px 5px 5px; background-color:#ffe8b8;}
-			input {width:100%; height:20px;}
-			.finalDiv {height:25px;}
-			.tallInput {height:30px;}
-			.cashHeader {margin:5px 5px -2px 5px;}
-			.cashFooter {margin:-2px 5px 5px 5px;}
-			.cashLabeltd {border:2px solid #000;}
-			.oddRow {background-color:#ffe8b8;}
-			.evenRow {background-color:#e8e8e8;}
-			.filler {background-color:#ff9c00; border:0;}
-			.headerFiller {background-color:#fff; border:0;}
-			#cashFlow {table-layout: fixed;}
-			#cashContainer {padding:5px; background-color:#ff9c00; width:63%; border: 2px solid #000;}
-			#countContainer {padding:5px; background-color:#ff9c00; width:35%; border: 2px solid #000; float:right;}
-			#overShortContainer {margin:0 auto; width:25%; background-color:#ff9c00;}
-			input[type="date"] {
-				position: relative;}
-
-			/* create a new arrow, because we are going to mess up the native one*/
-			input[type="date"]:after {
-			padding: 0 5px;
-			display:none;}
-
-
-			/* make the native arrow invisible and stretch it over the whole field so you can click anywhere 
-				in the input field to trigger the native datepicker*/
-			input[type="date"]::-webkit-calendar-picker-indicator {
-				position: absolute;
-				top: 0;
-				left: 0;
-				right: 0;
-				bottom: 0;
-				width: auto;
-				height: auto;
-				color: transparent;
-				background: transparent;}
-
-			/* adjust increase/decrease button */
-			input[type="date"]::-webkit-inner-spin-button {display: none;}
-
-			/* adjust clear button */
-			input[type="date"]::-webkit-clear-button {display:none;}
-			</style>
-            
-            <script>
-                function increaseDateByOne(){
-                    document.getElementById("date").stepUp(1);
-                    clearPage();   
-                }
-                function decreaseDateByOne(){
-                    document.getElementById("date").stepDown(1);
-                    clearPage();
-                }
-                function loadCurDate(){
-                    document.getElementById("date").valueAsDate = new Date();
-                }
-                window.onload = function(){
-                    loadCurDate();
-                }
-            </script>
-            
-			</header>
-          <div class="row">
-          	<div class="col-6">                
-				  <table border="0" cellspacing="0" cellpadding="0" class="table table-responsive table-sm">
-					  <tbody>
-						<tr>
-						  <td width="80%" class="border-all text-left">Target Advertising%</td>
-						  <td width="20%" class="bg-yellow border-all">&nbsp;</td>
-						</tr>
-						<tr>
-						  <td width="80%" class="border-all text-left">Target Cleaning Supplies%</td>
-						  <td width="20%" class="border-all">&nbsp;</td>
-						</tr>
-						<tr>
-						  <td width="80%" class="border-all text-left">Target Linen%</td>
-						  <td width="20%" class="bg-yellow border-all">&nbsp;</td>
-						</tr>
-                        <tr>
-						  <td width="80%" class="border-all text-left">Target Office Supplies%</td>
-						  <td width="20%" class="bg-yellow border-all">&nbsp;</td>
-						</tr>
-						<tr>
-						  <td width="80%" class="border-all text-left">Target Repairs and Maintenance%</td>
-						  <td width="20%" class="border-all">&nbsp;</td>
-						</tr>
-						<tr>
-						  <td width="80%" class="border-all text-left">Target Restaurant Supplies%</td>
-						  <td width="20%" class="bg-yellow border-all">&nbsp;</td>
-						</tr> 
-					  </tbody>
-					</table>
-		  	</div>
-			 <div class="col-6">               
-			   <table border="0" cellspacing="0" cellpadding="0" class="table table-responsive table-sm">
-				  <tbody>
-						<tr>
-                            <td width="75%" class="border-all text-left">Current Food Cost%</td>
-                            <td width="50%" class="bg-yellow border-all">&nbsp;</td>
-						</tr>
-                        <tr>
-                            <td width="75%" class="border-all text-left">Current Alcohol Cost%</td>
-                            <td width="50%" class="bg-yellow border-all">&nbsp;</td>
-						</tr>
-						<tr>
-						  <td width="75%" class="border-all text-left">Adjusted Budget</td>
-						  <td width="25%" class="border-all">&nbsp;</td>
-						</tr>
-						<tr>
-						  <td width="75%" class="border-all text-left">Remaining Budget</td>
-						  <td width="25%" class="bg-yellow border-all">&nbsp;</td>
-						</tr>
-                        <tr>
-						  <td width="75%" class="border-all text-left">Purchases</td>
-						  <td width="25%" class="bg-yellow border-all">&nbsp;</td>
-						</tr>
-				    </tbody>
-				</table>
-		  	</div>
-		  </div>
-         
-         <div class="col-12">
-			<form method="POST">
-                <div class="form-group w-50">                   
-					<select name="option" onload="displayProject(this.value);" onchange="displayProject(this.value);">                
-						<option value="">Select...</option>
-						<option value="food">Food Costs</option>
-						<option value="alcohol">Alcohol Costs</option>
-						<option value="advertising">Advertising</option>
-						<option value="cleaning">Cleaning Supplies</option>
-						<option value="linen">Linen</option>
-						<option value="office">Office Supplies</option>
-						<option value="repmaint">Repair and Maintenance</option>
-                       	<option value="restaurant">Restauraunt Supplies</option>
-                    </select> 
-                     <input  type="submit" name="submit" value="View Purchases">
-				</div>                        
-			</form> 
-		 <?php
-		if(isset($_POST['option'];
-		if($option == "alcohol")
-		{
-			$selectOption_query = SELECT vendor_name FROM alcohol_inventory
-				LEFT JOIN vendor ON vendor.vendor_id = alcohol_inventory.vendor_id;
-		$purchase_result = mysqli_query($connect, $selectOption_query);
-		}
-	}
-?>
-
-		    <table border="0" cellspacing="0" cellpadding="0" class="table table-bordered">
-			    <thead class="thead-dark">
-			        <tr class="text-center">
-			           <th width="16%">Date</th>
-			           <th width="16%">Vendor</th>
-			           <th width="16%">Amount</th>
-					</tr>
-				<?php 
-
-				
-					while($selectedOption = mysqli_fetch_array($purchase_result)) {
-					echo "<tr><td>" . $selectedOption['entry_date'] . "</td><td>" . $selectedOption['vendor_name'] . "</td><td>" . $selectedOption['cost'] . "</td></tr>";
-					}
-				
-				?>
-				</thead>
-		        
-		    </table>
+		</header>
+		<div class="row">
+          <div class="col-12">
+        	<form method="POST">
+                 <div class="form-group w-50">                   
+                    <select name="purchases" class="form-control" id="exampleFormControlSelect1">                
+			<option value="">Select...</option>
+                        <option value="food">Food Costs</option>
+                        <option value="alcohol">Alcohol Costs</option>
+			<option value="advertising">Advertising</option>
+			<option value="cleaning">Cleaning Supplies</option>
+			<option value="linen">Linen</option>
+			<option value="office">Office Supplies</option>
+			<option value="repair_maint">Repair and Maintenance</option>
+                        <option value="restaurant">Restauraunt Supplies</option>
+                    </select>
+                     <input class="btn btn-warning btn-sm" type="submit" name="submit" value="View Purchases">
+		</div>                        
+		</form>  
+                        <?php
+                        if(isset($_POST['submit']))
+                        {
+				$option = ($_POST['purchases']);
+			}
+		  	if($option == "food")
+			{
+                        $foodOption = "SELECT food_item_cost.*, cost, start_date, vendor_name 
+			FROM food_item_cost, vendor 
+			LEFT JOIN vendor ON vendor.vendor_id = food_item_cost.vendor_id";
+                        $purchase_result = mysqli_query($connect, $foodOption);
+                        }
+                       /* else if($option == alcohol")
+                        {
+                        $purchaseOption = "SELECT vendor_name FROM vendor";
+                        $purchase_result = mysqli_query($connect, $purchaseOption);
+                        }*/
+                        /* else if($option == "advertising")
+                        {
+                        }
+                        else if(($option == "cleaning")
+                        {
+                        }
+                        else if(($option == "linen")
+                        {
+                        }
+                        else if(($option == "repair_maint")
+                        {
+                        }
+                        else(($option == "restaurant")
+                        {
+                        }; */
+			
+                        ?>
+		  <?php
+			<tr>
+				while($selectedPurchase = mysqli_fetch_array($purchase_result)){
+                                        echo "<td>".$selectedPurchase['date']."</td><td>".$selectedPurchase['vendor_name']."</td><td>".$selectedPurchase['cost']."</td>";
+                                        }
+			</tr>
+		  ?>
 		</div>
-		</div>
+	</div>	
     </section>
     <!-- #about -->
 
