7e8c7207c4b5a880474baf6efd3757629c761256
gfossler <60489205+gfossler@users.noreply.github.com>
2020-03-05 20:16:21 -0600

    Update purchases.php

--- pages/purchases.php
+++ pages/purchases.php
@@ -185,13 +185,14 @@ if(!isset($_SESSION['user_id'])){
                         {
 				$option = ($_POST['purchases']);
 			}
-		  	if($P_POST['food'])
+		  	if($option == "food")
 			{
-                        $foodOption = "SELECT food_item_cost.*, cost, start_date, vendor_name FROM food_item_cost, vendor
-                        LEFT JOIN vendor ON vendor.vendor_id = food_item_cost.vendor_id";
+                        $foodOption = "SELECT food_item_cost.*, cost, start_date, vendor_name 
+			FROM food_item_cost, vendor 
+			LEFT JOIN vendor ON vendor.vendor_id = food_item_cost.vendor_id";
                         $purchase_result = mysqli_query($connect, $foodOption);
                         }
-                        if(isset($_POST['alcohol'))
+                        else if($option == alcohol")
                         {
                         $purchaseOption = "SELECT vendor_name FROM vendor";
                         $purchase_result = mysqli_query($connect, $purchaseOption);
