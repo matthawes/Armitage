9de629aa08c9cda85272d40d7fff114a6e936087
gfossler <60489205+gfossler@users.noreply.github.com>
2020-03-09 20:30:59 -0500

    Update dashboard2.php

--- pages/dashboard2.php
+++ pages/dashboard2.php
@@ -191,14 +191,18 @@ function toggleDataSeries(e) {
 	<tbody>   
             <tr>
 		    
-	    <?php
+	   <th class="text-left bg-warning2">Proj. Food</th>
+              <td class="bg-yellow"><input class="form-control1 bg-yellow2 text-right" type="text" value="2,500" id="pf1"></td>
+              <td class="bg-yellow"><input class="form-control1 bg-yellow2 text-right" type="text" value="3,000" id="pf2"></td>
+              <td class="bg-yellow"><input class="form-control1 bg-yellow2 text-right" type="text" value="3,200" id="pf3"></td>
+              <td class="bg-yellow"><input class="form-control1 bg-yellow2 text-right" type="text" value="3,500" id="pf4"></td>
+              <td class="bg-yellow"><input class="form-control1 bg-yellow2 text-right" type="text" value="5,500" id="pf5"></td>
+              <td class="bg-yellow"><input class="form-control1 bg-yellow2 text-right" type="text" value="5,000" id="pf6"></td>
+              <td class="bg-yellow"><input class="form-control1 bg-yellow2 text-right" type="text" value="2,800" id="pf7"></td>
+              <td class="bg-white2"><input class="form-control1 bg-white2 text-right" type="text" value="25,500.00" id="pft"></td>
+              <td class="bg-white2"><input class="form-control1 bg-white2 text-right" type="text" value="76.8%" id="pfp"></td>
+              </tr>
 
- 		/*$dashboard_query = "SELECT projected_food FROM dashboard_data WHERE dashboard_date BETWEEN DATEADD($ts)";
- 		$dashboard_result = mysqli_query($connect, $dashboard_query);
- 		$selecteddashboard = mysqli_fetch_array($dashboard_result);
-
-	    echo "<th class="text-left bg-warning2">Proj. Food</th><td class="bg-yellow"><input class="form-control1 bg-yellow2 text-right">".$selecteddashboard['projected_food']."</td>*/<td class="bg-white2"><input class="form-control1 bg-white2 text-right" type="text" value="25,500.00" id="pft"></td><td class="bg-white2"><input class="form-control1 bg-white2 text-right" type="text" value="76.8%" id="pfp"></td>"
-		?>
               </tr>
             <tr>
               <th class="text-left bg-warning2">Proj. Alcohol</th>
