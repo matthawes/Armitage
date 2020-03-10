092dd298f1c1d6846669b13f0af3099fc111fd91
gfossler <60489205+gfossler@users.noreply.github.com>
2020-03-09 21:10:28 -0500

    Update dashboard2.php

--- pages/dashboard2.php
+++ pages/dashboard2.php
@@ -12,7 +12,7 @@ if(!isset($_SESSION['user_id'])){
 	(int)$prevpage = $currentpage - 1;
 ?>
 <?php 
-$dashboard_query = "SELECT projected_food FROM dashboard_data WHERE dashboard_date BETWEEN CONVERT(DATETIME, .$ts.)";
+$dashboard_query = "SELECT projected_food FROM dashboard_data WHERE dashboard_date = CONVERT(date( .$ts.))";
 $dashboard_result = mysqli_query($connect, $dashboard_query);
 ?>
 <script type="text/javascript">
